4f7e7f53b242868a2a4af2577675a760c03cdc63
Calor21 <wellington.calor@gmail.com>
2022-05-19 19:52:29 +0100

    Pushed to repo

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ const App = () => {
     setList(newList);
   }
 
-  // Função feita para tarefinha de casa.
   const handleTaskChange = (id: number, done: boolean) => {
     let newList = [...list];
     for(let i in newList) {
